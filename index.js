798478e9867b8a684a83954afa14b4f28a6816e5
loun <loun@pop-os.localdomain>
2021-09-21 12:30:23 +0100

    update commit

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const shell = require("shelljs");
-const fs = require("fs");
-const scraper = require("./scraper");
+var cron = require('node-cron');
+var shell = require('shelljs');
 
-(async() => {
-    // intailiser le scraper
-    await scraper.initialize();
-
-    // recuperer tout les link des produits 
-    //let results_links = await scraper.getResults();
-
-
-    // recuperer tout les produits a partir des liens  
-    let results = await scraper.getInfos();
-})();
+cron.schedule('0 3 */2 * *', function() {
+    shell.exec("node updateproduct.js")
+});
